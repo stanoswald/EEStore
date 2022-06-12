9d0173b9a1cbe4d5a6dbbd5d387d060d72feab11
StanOswald <53464809+StanOswald@users.noreply.github.com>
2022-06-12 13:06:22 +0800

    封装ResponseEntity Builder

---
+++ src/main/java/cn/stanoswald/eestore/entity/CommonResponse.java
@@ -0,0 +1,73 @@
+package cn.stanoswald.eestore.entity;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import java.nio.charset.StandardCharsets;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class CommonResponse {
+
+    public static class Builder {
+        private HttpStatus status;
+        private String message;
+        private Map<String, Object> data;
+
+        private static final MediaType APPLICATION_JSON_UTF8 =
+                new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8);
+
+        public Builder() {
+            this.message = "";
+        }
+
+        public Builder(HttpStatus status) {
+            this.status = status;
+            this.message = "";
+        }
+
+        public Builder ok() {
+            this.status = HttpStatus.OK;
+            return this;
+        }
+
+        public Builder error() {
+            this.status = HttpStatus.BAD_REQUEST;
+            return this;
+        }
+
+        public Builder message(String message) {
+            this.message = message;
+            return this;
+        }
+
+        public Builder data(Map<String, Object> dataMap) {
+            data = dataMap;
+            return this;
+        }
+
+        public Builder data(String k, Object v) {
+            data = Map.of(k, v);
+            return this;
+        }
+
+
+        public Builder data(String dataString) {
+            data = Map.of("result", dataString);
+            return this;
+        }
+
+        public ResponseEntity<Object> build() {
+            assert status != null;
+            return ResponseEntity.status(status).contentType(APPLICATION_JSON_UTF8).body(
+                    new LinkedHashMap<String, Object>() {{
+                        put("code", status.value());
+                        put("message", message);
+                        put("data", data);
+                    }}
+            );
+        }
+    }
+
+}
