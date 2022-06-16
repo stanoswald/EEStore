dc3390013fcb7c9682515d4e7057a1b47fe21387
StanOswald <stan_oswald@qq.com>
2022-06-16 12:09:45 +0800

    添加事务管理配置

---
+++ src/main/java/cn/stanoswald/eestore/config/MyBatisPlusConfiguration.java
@@ -0,0 +1,9 @@
+package cn.stanoswald.eestore.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement
+@Configuration
+public class MyBatisPlusConfiguration {
+}
